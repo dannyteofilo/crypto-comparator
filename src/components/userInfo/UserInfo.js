ea19137ea8684f0a85de1a7f24d14b776ba77bce
Daniel Teófilo <danndevfull@gmail.com>
2022-01-20 00:36:44 -0600

    feat: styling user info component

--- src/components/userInfo/UserInfo.js
+++ src/components/userInfo/UserInfo.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 export const UserInfo = ({ user }) => {
-  const { name, lastName } = user;
+  const { name, lastName, email, phone } = user;
   return (
     <div className="card user-info">
-      <h2>
-        Welcome {name} {lastName} to Crypto comparator
-      </h2>
+      <h2>Welcome {name} to Crypto comparator</h2>
+      <p>
+        Name: <strong>{name}</strong>
+      </p>
+      <p>
+        Last Name: <strong>{lastName}</strong>
+      </p>
+      <p>
+        Email: <strong>{email}</strong>
+      </p>
+      <p>
+        Phone: <strong>{phone}</strong>
+      </p>
     </div>
   );
 };
